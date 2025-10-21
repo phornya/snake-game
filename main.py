a9cc2e0680c59c9361e514da1e7d28e26e6aee8d
YA.PHORN <YA.PHORN@PNC.LOCAL>
2025-10-21 11:00:15 +0700

    New missing

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ except Exception:
 main_game = Main(screen, game_font, apple_image=apple, sound_folder="sound")
 
 SCREEN_UPDATE = pygame.USEREVENT
-pygame.time.set_timer(SCREEN_UPDATE, 180)
+pygame.time.set_timer(SCREEN_UPDATE, 160)
 
 # Game loop
 while True:
